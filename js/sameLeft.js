0549388bdd00882f9d43cd090c47411bc387b6ee
DarkSharpness_XzY <76582120+DarkSharpness@users.noreply.github.com>
2021-03-26 19:46:11 +0800

    Create sameLeft.js

---
+++ js/sameLeft.js
@@ -0,0 +1,63 @@
+document.write(""+
+"    <header class='lyear-header text-center' style='background-image:url(images/left-bg.jpg);'>"+
+"  <div class='lyear-header-container'>"+
+"    <div class='lyear-mask'></div>"+
+"    <h1 class='lyear-blogger pt-lg-4 mb-0'><a href='index.html'>"+
+"      DarkSharpness</a></h1>"+
+"    <nav class='navbar navbar-expand-lg'>"+
+"    <a class='navbar-toggler' data-toggle='collapse' data-target='#navigation' aria-controls='navigation' aria-expanded='false' aria-label='Toggle navigation'>"+
+"      <div class='lyear-hamburger'>"+
+"        <div class='hamburger-inner'></div>"+
+"      </div>"+
+"    </a>"+
+"    "+
+"    <div id='navigation' class='collapse navbar-collapse flex-column'>"+
+"    <div class='profile-section pt-3 pt-lg-0'>"+
+"      <img class='profile-image mb-3 rounded-circle mx-auto' src='images/logo.png' width='120' height='120' alt='笔下光年' >"+
+"      <div class='lyear-sentence mb-3'>"+
+"        没啥好说的,有空就记录点想法."+
+"      </div>"+
+"        <hr>"+
+"    </div>"+
+"    "+
+"    <ul class='navbar-nav flex-column text-center'>"+
+"      <li class='nav-item active'>"+
+"        <strong>"+
+"        <a class='nav-link' href='index.html' style='color:rgb(255, 255, 137);'>"+
+"          首页"+
+"        </a>"+
+"      </li>"+
+"      <li class='nav-item'>"+
+"        <a class='nav-link' href='blog.html' style='color:rgb(255, 255, 137);'>"+
+"          博客"+
+"        </a>"+
+"      </li>"+
+"      <li class='nav-item' >"+
+"        <a class='nav-link' href='music.html' style='color:rgb(255, 255, 137);'>"+
+"          音乐"+
+"        </a>"+
+"      </li>"+
+"      <li class='nav-item' >"+
+"        <a class='nav-link' href='http://DarkSharpness.github.io/index.html' style='color:rgb(255, 255, 137);'>"+
+"          语言 language"+
+"        </a>"+
+"      </li>"+
+"      <li class='nav-item' >"+
+"        <a class='nav-link' href='about.html 'style='color:rgb(255, 255, 137);'>"+
+"          关于我"+
+"        </a>"+
+"      </strong>"+
+"      </li>"+
+"      "+
+"    </ul>"+
+"    "+
+"    <!--<div class='my-2 my-md-3'>"+
+"        <form class='lyear-search-form form-inline justify-content-center pt-3'>"+
+"          <input type='email' id='semail' name='semail1' class='form-control mr-md-1' placeholder='搜索关键词' />"+
+"        </form>"+
+"    </div>-->"+
+"  </div>"+
+"  </nav>"+
+"  </div>"+
+"</header>"+
+"}");
