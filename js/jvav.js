880a524c517328ae6bbb3c38c8c05072619d8a58
DarkSharpness_XzY <76582120+DarkSharpness@users.noreply.github.com>
2021-04-03 16:09:12 +0800

    Update jvav.js

--- js/jvav.js
+++ js/jvav.js
@@ -1,5 +1,5 @@
 document.write(""+
-"<script type=\"text/javascript\" src=\"js/jquery.min.js\"></script>"+
-"<script type=\"text/javascript\" src=\"js/jquery.nicescroll.min.js\"></script>"+
-"<script type=\"text/javascript\" src=\"js/bootstrap.min.js\"></script>"+
-"<script type=\"text/javascript\" src=\"js/main.min.js\"></script>");
+"<script type=\"text/javascript\" src=\"https://darksharpness.github.io/js/jquery.min.js \"></script>"+
+"<script type=\"text/javascript\" src=\"https://darksharpness.github.io/js/jquery.nicescroll.min.js \"></script>"+
+"<script type=\"text/javascript\" src=\"https://darksharpness.github.io/js/bootstrap.min.js \"></script>"+
+"<script type=\"text/javascript\" src=\"https://darksharpness.github.io/js/main.min.js \"></script>");
