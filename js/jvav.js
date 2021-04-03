ce9b8ebdd41b8cc139beb12acfc1fb3f36098ddc
DarkSharpness_XzY <76582120+DarkSharpness@users.noreply.github.com>
2021-04-03 16:02:54 +0800

    j modified

---
+++ js/jvav.js
@@ -0,0 +1,5 @@
+document.write(""+
+"<script type=\"text/javascript\" src=\"js/jquery.min.js\"></script>"+
+"<script type=\"text/javascript\" src=\"js/jquery.nicescroll.min.js\"></script>"+
+"<script type=\"text/javascript\" src=\"js/bootstrap.min.js\"></script>"+
+"<script type=\"text/javascript\" src=\"js/main.min.js\"></script>");
